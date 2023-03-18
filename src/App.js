9f4b9d6946d8b8f7c8d11f329517f5580220a60a
vicma <vmaldaraca@gmail.com>
2023-03-18 12:56:44 -0600

    Add bootstrap and add table to display greeting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
+import Greetings from './components/Greetings';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greetings />
     </div>
   );
 }
